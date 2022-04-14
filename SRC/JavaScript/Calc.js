4b1d1a369729db2c7026d8ee53f64cc327190a01
Zilvis0 <zilvinas.pusnys@gmail.com>
2022-04-14 15:12:24 +0300

    More calculator stuff

--- SRC/JavaScript/Calc.js
+++ SRC/JavaScript/Calc.js
@@ -1,13 +1,35 @@
 const NumberArea = document.getElementById("NumberArea")
 let FirstValue
+let PrevNumber
+let Symbol
+let isEqualsClicked = false
+let is4Clicked = false
+const AnswerArea = document.getElementById("AnswerArea")
 
 NumberArea.textContent = 0
+AnswerArea.textContent = 0
+
+function JhinMain(Curtain) {
+    is4Clicked = Curtain
+    if (is4Clicked == true) {
+        document.body.style.backgroundColor = "red";
+    } else document.body.style.backgroundColor = "blue";
+}
+
+
+if (NumberArea==4) {
+    document.body.style.backgroundColor = "green";
+}
+
 function AddNumber(number) {
     if (Number(NumberArea.textContent)==0) {
         NumberArea.textContent = number;
     } else {
     NumberArea.textContent += number;
     }
+    if (number==4) {
+        JhinMain(true)
+    } else JhinMain(false)
 }
 
 
@@ -17,17 +39,44 @@ function DeleteLast() {
         NumberArea.textContent=0
     }
 }
-function Addition() {
-    FirstValue = Number(NumberArea.textContent)
+// function Addition() {
+//     FirstValue = Number(NumberArea.textContent)
+//     NumberArea.textContent = 0
+// }
+// function Division() {
+//     FirstValue = Number(NumberArea.textContent)
+//     NumberArea.textContent = 0
+// }
+
+// function Equal() {
+//     SecondValue = NumberArea.textContent
+//     Answer = Number(FirstValue) + Number(SecondValue)
+//     NumberArea.textContent = Answer
+// }
+
+function MathAction(LastSymbol) {
+
+if (PrevNumber && !(isEqualsClicked)) {
+    Equals(false)
+    }
+
+    PrevNumber = Number(NumberArea.textContent)
     NumberArea.textContent = 0
-}
-function Division() {
-    FirstValue = Number(NumberArea.textContent)
-    NumberArea.textContent = 0
+    Symbol = LastSymbol
+    AnswerArea.textContent = PrevNumber
 }
 
-function Equal() {
-    SecondValue = NumberArea.textContent
-    Answer = Number(FirstValue) + Number(SecondValue)
-    NumberArea.textContent = Answer
+
+function Equals(clicked) {
+    isEqualsClicked = clicked
+if (Symbol == '+') {
+    NumberArea.textContent = PrevNumber + Number(NumberArea.textContent)
+    } else if (Symbol == '-') {
+        NumberArea.textContent = PrevNumber - Number(NumberArea.textContent)
+    } else if (Symbol == '/') {
+        NumberArea.textContent = PrevNumber / Number(NumberArea.textContent)
+    } else if (Symbol == '*') {
+        NumberArea.textContent = PrevNumber * Number(NumberArea.textContent)
+    }
+    AnswerArea.textContent = NumberArea.textContent
 }
