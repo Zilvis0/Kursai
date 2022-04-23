3a0c792c31ef3d1f752e72934eba7be8ea1fbc19
Zilvis0 <zilvinas.pusnys@gmail.com>
2022-04-23 13:40:32 +0300

    Almost Done

--- SRC/JavaScript/Objects.js
+++ SRC/JavaScript/Objects.js
@@ -69,14 +69,13 @@ const group = [
         hobby: "Bassketball",
         height: 185,
         programmingSkill: 4
-    
     }
 ]
 
 const keys = Object.keys(group[0])
-function createTable() {
+function createTable(reductions) {
     TableBody.innerHTML = ""
-    group.map(obj => {
+    reductions.map(obj => {
     const tableRow = document.createElement("tr")
     for (objkey of keys){
         const tableData = document.createElement("td")
@@ -88,11 +87,68 @@ function createTable() {
     maximumRow()
 }
 
-createTable()
 
 
-// function maximumRow() {
-//     const tableRow = document.createElement("tr")
-//     maxValue = Object.values(group).
-    
-// }
+
+function maximumRow() {
+    const tableRow = document.createElement("tr")
+    function SpecificValue(value){
+        return Math.max(...group.map(person => Number(person[value])))
+    }
+   
+    for (let param of keys) {
+        const tableData = document.createElement("td")
+        tableData.classList.add("fw-bold")
+        if(param == 'height' || param == 'age' || param == 'programmingSkill'){
+            tableData.textContent = SpecificValue(param)
+            tableData.classList.add("fw-bold", "border-dark", "border-top", "border-2")
+        }
+        tableRow.appendChild(tableData)
+    }
+    TableBody.appendChild(tableRow)
+}
+
+function Sort(value){
+
+    if (value == 'firstName' || value == 'lastName' || value == 'gender' || value == 'hobby'){
+    const OrganisedByLetters = group.sort((a, b)=>a[value].localeCompare(b[value]))
+    createTable(OrganisedByLetters)
+    console.log(OrganisedByLetters)
+    } else if (value == 'age' || value == 'height' || value == 'programmingSkill'){
+    const OrganisedByNumbers = group.sort((a, b)=>a[value] - b[value])
+    createTable(OrganisedByNumbers)
+    console.log(OrganisedByNumbers)
+    } else {
+        const DefaultSorting = group.sort((a, b)=>a.firstName.localeCompare(b.firstName))
+        createTable(DefaultSorting)
+        console.log(DefaultSorting)
+    }
+}
+
+function Reducing(value){
+    if (value == 'All'){
+        createTable(group)
+    } else if (value == 'Males'){
+        const Males = group.filter(person => person.gender == "Male")
+        createTable(Males)
+    } else if (value == 'Females'){
+        const Females = group.filter(person => person.gender == "Female")
+        createTable(Females)
+    } else if (value == 'Youngsters'){
+        const Youngsters = group.filter(person => person.age < 30)
+        createTable(Youngsters)
+    } else if (value == 'TallBois'){
+        const TallBois = group.filter(person => person.height > 175)
+        createTable(TallBois)
+    } else if (value == '!Hackers'){
+        const NotHackers = group.filter(person => person.programmingSkill < 5)
+        createTable(NotHackers)
+    } else {
+        createTable (group)
+    }
+}
+
+
+
+
+createTable(group)
