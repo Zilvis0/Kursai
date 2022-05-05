0ef73006fd90e2973c02f850375637d1a82694c6
Zilvis0 <zilvinas.pusnys@gmail.com>
2022-05-05 17:11:17 +0300

    Breaktrough! Search is working

--- SRC/JavaScript/Fakebook.js
+++ SRC/JavaScript/Fakebook.js
@@ -13,14 +13,36 @@ let userInfo = document.getElementById("userInfo")
 let feed = document.getElementById("feed")
 let feedWrapper = document.getElementById("feedWrapper")
 let organiserName = document.getElementById("organiser")
-let organiserPhoto = document.getElementById("organiserPhoto")
 let organisersPool
+let joinButton = document.getElementById("joinButton")
+let roomArea = document.getElementById("roomArea")
+let searchElement = document.getElementById("searchInput")
+let searchResults = document.getElementById("searchResults")
 
 function getPeople (person) {
     fetch(`${baseUrl + people100}`)
     .then((response) => response.json()
     .then((data) => {
         people = data.results;
+
+        searchElement.addEventListener("keyup", (event) => {
+        
+        
+            const a = people.filter(person => person.name.first.startsWith(event.target.value))
+            searchResults.innerHTML = ""
+            console.log(event.target.value)
+        if (a == ""){
+            searchResults.innerHTML += `<div>Sorry, No Results Found</div>` 
+        }else if( event.target.value ==""){
+            searchResults.innerHTML += `<div>Start Typing Name</div>`
+        } else {
+            a.map(object=>{
+                
+                searchResults.innerHTML += `<div>${object.name.first} ${object.name.last}</div>`
+            })
+            console.log()
+        }
+        })
 
         userPhoto.src = people[person].picture.large
         userInfo.innerHTML = `<div>${people[person].name.first}</div>
@@ -32,34 +54,28 @@ function getPeople (person) {
 
 getPeople(0, "large")
 
-let activity
-let type
-let participants
-
-
-
-// const newFeed = document.createElement("div")
-// for (let i=1; i<6; i++){
-//     newFeed.appendChild(getPeopleFeed(i))
-// }
-
-function getPeopleFeed(person){
+const newFeed = document.createElement("div")
+for (let i=1; i<=6; i++){
     fetch(`${baseUrl + people100}`)
-    .then((response) => response.json()
-    .then((data) => {
-        organisersPool = data.results;
-        organiserPhoto.src = organisersPool[person].picture.thumbnail;
-        organiserName.innerHTML = `<div>${organisersPool[person].name.first}</div><div>${organisersPool[person].name.last}</span>`
-    }))
+        .then((response) => response.json()
+        .then((data) => {
+            organisersPool = data.results;
+            organiserName.innerHTML+=`<img src="${organisersPool[i].picture.thumbnail}">
+            <div>${organisersPool[i].name.first}</div><div>${organisersPool[i].name.last}</div>`
+            joinButton.innerHTML += `<button class="btn btn-outline-info" onclick="${()=>Room(i)}">Join activity</button>`
+        }))
     fetch(feedInfo)
-    .then((response) => response.json()
-    .then((data) => {
-        activity = data.activity;
-        type = data.type;
-        participants = data.participants;
-        feed.innerHTML = `<div>Activity: ${activity}</div><div>Type: ${type}</div><div>Participants: ${participants}</div>`
-    }))
+        .then((response) => response.json()
+        .then((data) => {
+            feed.innerHTML += `<div>Activity: ${data.activity}</div><div>Type: ${data.type}</div><div>Participants: ${data.participants}</div>`
+        }))
 }
-  
 
-getPeopleFeed(1)
+function Room(param){
+  
+    roomArea.innerHTML+=`<img src="${organisersPool[param].picture.thumbnail}">
+    <div>${organisersPool[param].name.first}</div><div>${organisersPool[param].name.last}</div>`
+    console.log("hello")
+}   
+
+
